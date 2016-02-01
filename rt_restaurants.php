6aa6eb3ec683fbf583d53484f86f1fe64f81266e
admin <admin@admin.admin>
2016-02-01 12:30:35 +0530

    added plugin headers

--- rt_restaurants.php
+++ rt_restaurants.php
@@ -1,10 +1,16 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*
+Plugin Name: rtRestaurants
+Plugin URI:  http://rtcamp.com
+Description: Restaurat directory
+Version:     0.1
+Author:      vaishuagola27
+Author URI:  http://rtcamp.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: rt-restaurants
+*/
 
 // Custom code starts here
 
